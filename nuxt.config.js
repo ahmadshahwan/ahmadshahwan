b43f7d6ce5be1903a4602b82e6789b497b1ffa48
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-10-30 17:28:38 +0100

    Add favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/style/main.css', ],
+    css: ['~/assets/style/main.css',],
+    app: {
+        head: {
+            title: "Ahmad SHAHWAN pages",
+            link: [{rel: 'shortcut icon', type: 'image/x-icon', href: "/favicon.ico"}],
+        },
+    },
 })
