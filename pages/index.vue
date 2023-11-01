26ef36f5167bc1013c238b6798290e37a9c66a9b
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-01 15:05:35 +0100

    Update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,11 @@
         <div class="entry">
             <img src="/images/pigeons.jpg" alt="" width="112" height="131" class="alignleft" />
             <p>Ahmad Shahwan is software architect and full-stack developer who is willing to unfold opportunities, share expertise and exchange ideas.</p>
-            <p>Ahmad worked on the design, the development and the maintenance of robust digital solutions for clients such as
-              <a href="https://www.sncf.com" target="_blank">SNCF</a>, <a href="https://www.malt.fr" target="_blank">Malt</a>, <a href="https://www.rtone.fr" target="_blank">Rtone</a> and others</p>
-            <p>As a lecturer at <a href="https://www.univ-lyon1.fr" target="_blank">Claude Bernard University Lyon 1</a>, Ahmad is responsible for courses on Object-Oriented Modeling and Programming.</p>
-            <p>He earned his PH. D. in mathematics and coputer science from <a href="https://www.univ-grenoble-alpes.fr/" target="_blank">Grenoble Alpes University</a>.</p>
+            <p>Ahmad worked on designing, realizing and maintaining robust digital solutions for clients such as
+              <a href="https://www.sncf.com" target="_blank">SNCF</a>, <a href="https://www.malt.fr" target="_blank">Malt</a>, <a href="https://www.rtone.fr" target="_blank">Rtone</a> and others.</p>
+            <p>As a lecturer at <a href="https://www.univ-lyon1.fr" target="_blank">Claude Bernard University Lyon 1</a>, Ahmad is responsible for courses on Object-Oriented Modeling and Programming.
+            He teaches subjects such as Java programming language, UML and Design Patterns.</p>
+            <p>His thesis on geometrical analysis of digital mock-ups and function knowledge representation of 3D models earned him a PhD degree in Mathematics and Computer Science from <a href="https://www.univ-grenoble-alpes.fr/" target="_blank">Grenoble Alpes University</a>.</p>
             <br clear="left"/>
           <h3>Interests and Activities</h3>
           <table>
@@ -20,18 +21,6 @@
             <tr>
               <td>Cultural</td>
               <td>Painting and drawing.</td>
-            </tr>
-            <tr>
-              <td>Social</td>
-              <td>Member of the <a href="http://www.scouts.org.sy/" target="_blank">Syrian Scouts Association</a>.</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td><a href="http://www.amnesty.org/" target="_blank">Amnesty International</a> member (France).</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td><a href="http://www.ieee.org/" target="_blank">IEEE</a> student member (Region 8).</td>
             </tr>
           </table>
         </div>
