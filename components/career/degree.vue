5396a08ce1bc24106342c6df6b9a4e433da8aed6
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-01 22:14:35 +0100

    Review career page

---
+++ components/career/degree.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="degree">
+    <div>
+      <div class="year">
+        <slot name="year"></slot>
+      </div>
+      <div class="academic-title">
+        <slot name="academic-title"></slot>
+      </div>
+    </div>
+    <div>
+      <div>&nbsp;</div>
+      <div class="institution">
+        <slot name="institution"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.degree {
+  display: flex;
+  flex-direction: column;
+  padding-bottom: .75em;
+
+  > div {
+    display: flex;
+    flex-direction: row;
+
+    > div:first-child {
+      width: 15em;
+    }
+
+    .academic-title {
+      font-weight: bold;
+
+    }
+  }
+}
+</style>
---
+++ components/career/experience.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="experience">
+    <div class="header">
+    <span class="period">
+      <slot name="period"></slot>
+    </span>
+      <span class="job-title">
+      <slot name="job-title"></slot>
+    </span>
+      <span class="client">
+      <slot name="partner"></slot>
+    </span>
+    </div>
+    <div class="description">
+      <slot name="description"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.experience {
+  display: flex;
+  flex-direction: column;
+
+  .header {
+    display: flex;
+    flex-direction: row;
+
+    .period {
+      order: 3;
+      margin-left: auto;
+      font-weight: bold;
+    }
+
+    .job-title {
+      order: 1;
+      width: 20em;
+      font-weight: bold;
+    }
+
+    .client {
+      order: 2;
+    }
+  }
+
+  .description {
+    padding-bottom: .75em;
+  }
+}
+</style>
