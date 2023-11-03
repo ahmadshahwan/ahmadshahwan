85219215b8c5fe20f544ec97d9abde552ceb5151
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-03 16:59:13 +0100

    Add icons to experiences

--- components/career/experience.vue
+++ components/career/experience.vue
@@ -1,49 +1,69 @@
 <template>
   <div class="experience">
-    <div class="header">
-    <span class="period">
-      <slot name="period"></slot>
-    </span>
-      <span class="job-title">
-      <slot name="job-title"></slot>
-    </span>
-      <span class="client">
-      <slot name="partner"></slot>
-    </span>
-    </div>
-    <div class="description">
-      <slot name="description"></slot>
+    <img class="thumbnail" :src="imageUrl" :alt="props.name"/>
+    <div class="details">
+      <div class="header">
+        <span class="period">
+          <slot name="period"></slot>
+        </span>
+          <span class="job-title">
+          <slot name="job-title"></slot>
+        </span>
+          <span class="client">
+          <slot name="partner"></slot>
+        </span>
+      </div>
+      <div class="description">
+        <slot name="description"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 
+type Props = {
+  name: string;
+  image?: string;
+};
+
+const props = defineProps<Props>();
+const imageUrl = props.image ?? '/images/career/digamma.png';
 </script>
 
 <style scoped>
 .experience {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
 
-  .header {
-    display: flex;
-    flex-direction: row;
+  .thumbnail {
+    height: 32px;
+    width: 32px;
+    padding: 1px 10px 1px 1px;
+  }
 
-    .period {
-      order: 3;
-      margin-left: auto;
-      font-weight: bold;
-    }
+  .details {
 
-    .job-title {
-      order: 1;
-      width: 20em;
-      font-weight: bold;
-    }
+    .header {
+      display: flex;
+      flex-direction: row;
 
-    .client {
-      order: 2;
+      .period {
+        order: 3;
+        margin-left: auto;
+        font-weight: bold;
+      }
+
+      .job-title {
+        order: 1;
+        width: 20em;
+        font-weight: bold;
+        padding-bottom: 2px;
+      }
+
+      .client {
+        order: 2;
+      }
     }
   }
 
