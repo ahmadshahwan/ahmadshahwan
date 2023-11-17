f7cb6cbe9e828afd735bb2722c6b5ae19863a226
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-17 22:01:13 +0100

    Componentize default layout

---
+++ components/footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="footer">
+    <p>
+      <a target="_blank" href="http://www.linkedin.com/profile/view?id=57163147">
+        <img alt="" width="16" height="16" src="/images/linkedin.png"/>
+      </a>
+      &nbsp;
+      <a target="_blank" href="https://www.researchgate.net/profile/Ahmad_Shahwan">
+        <img alt="" width="16" height="16" src="/images/researchgate.png"/>
+      </a>
+      &nbsp;
+      <a target="_blank" href="http://twitter.com/#!/ahmadshahwan">
+        <img alt="" width="16" height="16" src="/images/twitter.png"/>
+      </a>
+      &nbsp;
+      <a target="_blank" href="http://www.facebook.com/ahmad.shahwan">
+        <img alt="" width="16" height="16" src="/images/facebook.ico"/>
+      </a>
+    </p>
+    <p>
+      17 rue Antoine Lumière,
+      69008 Lyon,
+      France
+    </p>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+#footer {
+  width: 920px;
+  height: 100px;
+  margin: 0px auto;
+  padding: 30px 0 0 0;
+  background: url(/images/bg07.jpg) no-repeat left top;
+}
+
+#footer p {
+  margin: 0;
+  text-align: center;
+  font-size: 11px;
+}
+
+#footer a {
+  color: #797979;
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,6 @@
 <template>
   <div id="wrapper">
-    <div id="header">
-      <div id="logo">
-        <h1>Ahmad <span class="sc">Shahwan</span></h1>
-        <h2>Research and Development Engineer</h2>
-      </div>
-      <!-- end #logo -->
-      <div id="menu">
-        <ul>
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/career">Career</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/teaching">Teaching</NuxtLink>
-
-          </li>
-          <li>
-            <NuxtLink to="/research">Research</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/events">Events</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/contacts">Contacts</NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <!-- end #menu -->
-    </div>
-    <!-- end #header -->
-
+    <Header></Header>
     <div id="page">
       <div id="bgtop">
         <div id="bgbottom">
@@ -41,39 +9,12 @@
             <slot/>
 
           </div>
-          <!-- end #content -->
-          <sidebar></sidebar>
+          <Sidebar></Sidebar>
           <div style="clear: both;">&nbsp;</div>
         </div>
       </div>
     </div>
-    <!-- end #page -->
-    <div id="footer">
-      <p>
-        <a target="_blank" href="http://www.linkedin.com/profile/view?id=57163147">
-          <img alt="" width="16" height="16" src="/images/linkedin.png"/>
-        </a>
-        &nbsp;
-        <a target="_blank" href="https://www.researchgate.net/profile/Ahmad_Shahwan">
-          <img alt="" width="16" height="16" src="/images/researchgate.png"/>
-        </a>
-        &nbsp;
-        <a target="_blank" href="http://twitter.com/#!/ahmadshahwan">
-          <img alt="" width="16" height="16" src="/images/twitter.png"/>
-        </a>
-        &nbsp;
-        <a target="_blank" href="http://www.facebook.com/ahmad.shahwan">
-          <img alt="" width="16" height="16" src="/images/facebook.ico"/>
-        </a>
-      </p>
-      <p>
-        17 rue Antoine Lumière,
-        69008 Lyon,
-        France
-      </p>
-
-    </div>
-    <!-- end #footer -->
+    <Footer></Footer>
   </div>
 </template>
 
@@ -82,5 +23,23 @@
 </script>
 
 <style scoped>
+#wrapper {
+  background: url(/images/bg02.jpg) repeat-x left top;
+}
+
+#bgtop {
+  background: url(/images/bg05.jpg) no-repeat left top;
+}
+
+#bgbottom {
+  background: url(/images/bg06.jpg) no-repeat left bottom;
+}
+
+#content {
+  float: left;
+  width: 650px;
+  padding: 50px 0 0 35px;
+  min-height: 400px;
+}
 
 </style>
