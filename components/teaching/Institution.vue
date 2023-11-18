1a746ebaf8cdb4c5e6928c64729422ef1276811a
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-18 19:15:59 +0100

    Componentize teaching

---
+++ components/teaching/Institution.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>{{ props.title }}</h3>
+  <slot></slot>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  title: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/teaching/Topic.vue
@@ -0,0 +1,16 @@
+<template>
+  <h4>{{ props.title }}</h4>
+  <slot></slot>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  title: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+
+</style>
